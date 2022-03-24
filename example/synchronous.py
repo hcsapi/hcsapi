4b50e249c7c5d40e73a424015bf2750bf07528fe
OUTSTANDAIM <62782800+scottjsh@users.noreply.github.com>
2022-03-24 23:45:38 +0900

    오류 수정

--- example/synchronous.py
+++ example/synchronous.py
@@ -7,6 +7,6 @@ region = input("지역을 입력하세요(예: 서울, 경기, 전남....): ")
 school = input("학교이름을 입력하세요(예: 두둥실고): ")
 password = input("비밀번호를 입력하세요: ")
 selfcheck = input("신속항원검사 사용 여부를 입력하세요(Yes=1 , No=0): ")
-data = selfcheck(name,birth,region,school,level,password,int(selfcheck))
+data = hcsapi.selfcheck(name,birth,region,school,level,password,selfcheck)
 
 print(data)
