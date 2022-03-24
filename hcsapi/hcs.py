a23e0ba55d7e67d5b109803003dd75c10fde0d86
OUTSTANDAIM <62782800+scottjsh@users.noreply.github.com>
2022-03-24 23:14:34 +0900

    Update hcs.py

--- hcsapi/hcs.py
+++ hcsapi/hcs.py
@@ -82,7 +82,7 @@ async def asyncSelfCheck(
     schoolname: str,
     level: str,
     password: str,
-    selfcheck: str = "0",
+    selfcheck: str,
     customloginname: str = None,
 ):
     async with aiohttp.ClientSession() as session:
